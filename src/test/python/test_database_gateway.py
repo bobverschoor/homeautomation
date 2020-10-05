2b7d21e71fa38f963c1f361300fb881dafc307d3
bob <bob.verschoor@alliander.com>
2020-10-05 06:35:40 +0200

    fix

--- src/test/python/test_database_gateway.py
+++ src/test/python/test_database_gateway.py
@@ -7,7 +7,7 @@ from persistence.database_gateway import DatabaseGateway
 class TestDatabasegateway(unittest.TestCase):
     def test_store_electra(self):
         electra = Electra()
-        electra.richting = Electra.HOOGTARIEF
+        electra.richting = Electra.VERBRUIKT
         electra.tarief = Electra.LAAGTARIEF
         electra.wh = 34224
         db = DatabaseGateway()
