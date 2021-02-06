7ae65218a1a81b36409d2d9fdede3ce4fda2f75f
bob <bob.verschoor@alliander.com>
2021-02-06 18:52:39 +0100

    fix init P1

--- src/main/python/sensor/p1_device.py
+++ src/main/python/sensor/p1_device.py
@@ -22,7 +22,7 @@ class P1Device:
             for richting in [P1Device.E_CONSUMED, P1Device.E_PRODUCED]:
                 e[tarief][richting] = packet['kwh'][tarief][richting]
         g = {P1Device.G_M3: packet['gas']['total'], P1Device.G_TIMESTAMP: packet['gas']['measured_at']}
-        data = {'electra': e, 'gas': g}
+        data = {"electra": e, 'gas': g}
         return str(data)
 
 #{"electra": {"low": {"consumed": 16351.117, "produced": 1.804}, "high": {"consumed": 20121.471, "produced": 0.007}}, "gas": {"m3": 7368.67, "timestamp": 1601391600}}
