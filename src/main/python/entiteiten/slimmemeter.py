dadc7f57ae659cc051b3c1f319deb9d9362b555d
bob <bob.verschoor@alliander.com>
2021-04-05 07:33:07 +0200

    tags met meternaam

--- src/main/python/entiteiten/slimmemeter.py
+++ src/main/python/entiteiten/slimmemeter.py
@@ -1,6 +1,9 @@
 class Slimmemeter():
     def __init__(self, header):
+        header = header.strip()
+        header = header.strip('//')
         self._naam, self._type = header.split('\\')
+
 
     @property
     def naam(self):
