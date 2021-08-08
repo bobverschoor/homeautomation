c3d732637fd534dc87860660ef749173a1ee98b0
bob <bob.verschoor@alliander.com>
2021-08-08 11:01:12 +0200

    added dryrun modus to not store in database

--- src/main/python/weerdata.py
+++ src/main/python/weerdata.py
@@ -1,3 +1,4 @@
+import argparse
 import configparser
 import datetime
 import os
@@ -9,14 +10,16 @@ from sensor.weerlive_api import WeerLiveDevice
 
 
 class WeerdataController:
-    def __init__(self, configfile):
+    def __init__(self, configfile, dryrun=False):
+        self._store_in_database = not dryrun
         self._config = configparser.ConfigParser()
         if os.path.exists(configfile):
             self._config.read(configfile)
             self._weerdata = WeerGateway()
             self._weerdata.weer_device = WeerLiveDevice(self._config)
             self._weerdata.neerslag_device = WeerhuisjeDevice(self._config)
-            self._databasebase = DatabaseGateway("weer")
+            if self._store_in_database:
+                self._databasebase = DatabaseGateway("weer")
         else:
             print("Config file does not exist: " + str(configfile) + ", cwd: " + os.getcwd())
             exit(1)
@@ -24,10 +27,18 @@ class WeerdataController:
     def collect_store(self):
         meetwaarden = self._weerdata.get_meetwaarden()
         for meetwaarde in meetwaarden:
-            self._databasebase.entiteiten = meetwaarde
-            self._databasebase.store()
+            if self._store_in_database:
+                self._databasebase.entiteiten = meetwaarde
+                self._databasebase.store()
+            else:
+                print(meetwaarde)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Collect weather data and stores it in a database.')
+    parser.add_argument('--dryrun', help='collect data but do not store in database.', action="store_true")
+    args = parser.parse_args()
+    if args.dryrun:
+        print("dryrun mode" )
     print(datetime.datetime.now())
-    WeerdataController('src/main/resources/secrets.ini').collect_store()
+    WeerdataController('src/main/resources/secrets.ini', dryrun=args.dryrun).collect_store()
