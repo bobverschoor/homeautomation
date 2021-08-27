023b85b71e9cc1af4707de4a149480044e5a9482
bob <bob.verschoor@alliander.com>
2021-08-27 16:46:25 +0200

    Control file added, using pidfile to check if already running. This way you can use cron to start it up.

--- src/main/python/deurbel.py
+++ src/main/python/deurbel.py
@@ -1,5 +1,8 @@
+import argparse
 import configparser
+import datetime
 import os
+from pid import PidFile
 
 from sensor.deurbel_gateway import DeurbelGateway
 
@@ -11,8 +14,7 @@ class DeurbelController:
         self._config = configparser.ConfigParser()
         if os.path.exists(configfile):
             self._config.read(configfile)
-            self._deurbel = DeurbelGateway()
-            self._deurbel.set_deurbel(self._config)
+            self._deurbel = DeurbelGateway(self._config)
             # if self._store_in_database:
             #     self._databasebase = DatabaseGateway(self._config['p1meter']['databasenaam'])
             # else:
@@ -21,3 +23,18 @@ class DeurbelController:
             print("Config file does not exist: " + str(configfile) + ", cwd: " + os.getcwd())
             exit(1)
 
+    def control_loop(self):
+        while True:
+            try:
+                if self._deurbel.someone_at_the_deur():
+                    print("stuur een berichtje")
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    with PidFile():
+        parser = argparse.ArgumentParser(description='Start the waiting for deurbel.')
+        args = parser.parse_args()
+        print(datetime.datetime.now())
+        DeurbelController('src/main/resources/secrets.ini', dryrun=args.dryrun).control_loop()
